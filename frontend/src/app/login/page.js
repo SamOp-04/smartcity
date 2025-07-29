80e67a8ce77bd271c9607084885e286aef18b4e1
SamOp-04 <146644053+SamOp-04@users.noreply.github.com>
2025-07-29 18:15:29 +0530

    Fix build errors and update schema changes

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { useRouter } from 'next/navigation'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect} from 'react'
 import Link from 'next/link'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import {
